b2673ea86987209a7057cd13597992a66075972d
RightSexyOrc <rightsexyorc@protonmail.com>
2024-02-01 21:02:56 -0500

    .aba .aba_keys, aba console command

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
+DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.aba/mainnet"))).resolve()
 
-DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
+DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.aba_keys"))).resolve()
 
-SIMULATOR_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_SIMULATOR_ROOT", "~/.chia/simulator"))).resolve()
+SIMULATOR_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_SIMULATOR_ROOT", "~/.aba/simulator"))).resolve()
